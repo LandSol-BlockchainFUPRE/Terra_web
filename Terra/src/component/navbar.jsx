11cd3a80ef8bf1268fbfc855026f3874c8d69407
sirisenju <amrohore4real@gmail.com>
2025-05-12 13:26:26 +0100

    initial commit done/home page done

---
+++ Terra/src/component/navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const NavBar = () => {
+  return (
+    <div className='w-[75%] h-[50px] bg-purple-500 mx-auto flex justify-between items-center p-2'>
+        <h1 className='text-4xl font-bold'>Terra</h1>
+        <div className='flex gap-4'>
+            <button className='px-7 py-2 bg-red-500 rounded-2xl'>Sing up</button>
+            <button className='px-7 py-2 bg-green-900 rounded-2xl'>Connect Wallet</button>
+        </div>
+    </div>
+  )
+}
+
+export default NavBar
---
+++ Terra/src/pages/Home.jsx
@@ -0,0 +1,51 @@
+import React from 'react'
+import NavBar from '../component/navbar'
+import Accordian from '../component/accordian'
+import accordianData from '../lib/accordianData'
+
+const Home = () => {
+  return (
+    <div className='hero-bg w-full h-screen'>
+        <div className='mx-auto'>
+            <NavBar/>
+        </div>
+
+        {/* center hero text */}
+        <div className='text-black w-[70%] mx-auto bg-white mt-[5%]'>
+            <div className='text-center p-4'>
+                <h1 className='text-5xl'>Welcome to <span>Terra</span>.</h1>
+                <p className='p-3'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsum incidunt debitis praesentium 
+                    at explicabo sed tempora perspiciatis beatae? Placeat, tenetur?
+                </p>
+            </div>
+
+            <div className='w-[50%] mx-auto'>
+                <div className='flex gap-2'>
+                    <div className='w-full'>
+                        <input type="file" className='p-2 border-2 border-black rounded-md w-full'/>
+                    </div>
+                    <button className='bg-green-400 px-12 py-2 rounded-md text-white'>Upload</button>
+                </div>
+                <div className='mt-4 flex gap-4'>
+                    <button className='bg-green-400 px-12 py-3 rounded-md text-white'>Transfar Ownership</button>
+                    <button className='bg-green-400 px-12 py-3 rounded-md text-white'>View Document</button>
+                </div>
+            </div>
+
+            {/* accordian */}
+            <div className="accordion mx-auto pt-[10%]">
+              {accordianData.map(({ title, message, id, number }) => (
+                <Accordian
+                  title={title}
+                  message={message}
+                  key={id}
+                  number={number}
+                />
+              ))}
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Home
