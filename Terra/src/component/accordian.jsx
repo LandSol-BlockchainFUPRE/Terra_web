adedc79ef78460eb1e47ef8af9666f739aa8b9ea
sirisenju <amrohore4real@gmail.com>
2025-05-15 16:04:09 +0100

    component added

--- Terra/src/component/accordian.jsx
+++ Terra/src/component/accordian.jsx
@@ -14,7 +14,7 @@ const Accordian = ({title, message, number}) => {
               onClick={() => setIsActive(!isActive)}
             >
                 <div className="flex gap-x-10">
-                    <div className="text-xl font-semibold text-[#2405F2]">{number}</div>
+                    <div className="text-xl font-semibold text-white">{number}</div>
                     <div className="text-lg font-medium">{title}</div>
                 </div>
               <div>{isActive ? "-" : "+"}</div>
--- Terra/src/pages/Home.jsx
+++ Terra/src/pages/Home.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 import NavBar from '../component/navbar'
 import Accordian from '../component/accordian'
 import accordianData from '../lib/accordianData'
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog"
+
 
 const Home = () => {
+
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <div className='hero-bg w-full h-screen'>
         <div className='mx-auto'>
@@ -11,7 +23,7 @@ const Home = () => {
         </div>
 
         {/* center hero text */}
-        <div className='text-white w-[65%] mx-auto mt-[5%] rounded-4xl shadow-md glass'>
+        <div className='text-white w-[65%] mx-auto mt-[2%] pb-[2%] rounded-4xl shadow-md glass'>
             <div className='text-center p-4'>
                 <h1 className='text-5xl'>Welcome to <span>Terra</span>.</h1>
                 <p className='p-3'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsum incidunt debitis praesentium 
@@ -19,16 +31,17 @@ const Home = () => {
                 </p>
             </div>
 
-            <div className='w-[50%] mx-auto'>
+            <div className='w-[70%] mx-auto'>
                 <div className='flex gap-2'>
                     <div className='w-full'>
-                        <input type="file" className='p-2 border-2 border-black rounded-md w-full'/>
+                        <input type="file" className='p-2 border-2 border-white rounded-md w-full'/>
                     </div>
-                    <button className='bg-green-400 px-12 py-2 rounded-md text-white'>Upload</button>
+                    <button onClick={() => setIsOpen(true)} className='bg-green-400 px-12 py-2 rounded-md text-white'>Upload</button>
                 </div>
                 <div className='mt-4 flex gap-4'>
-                    <button className='bg-green-400 px-12 py-3 rounded-md text-white'>Transfar Ownership</button>
-                    <button className='bg-green-400 px-12 py-3 rounded-md text-white'>View Document</button>
+                    <button className='bg-green-400 px-12 py-2 rounded-md text-white'>Transfar Ownership</button>
+                    <button className='bg-green-400 px-12 py-2 rounded-md text-white'>Marketplace</button>
+                    <button className='bg-green-400 px-12 py-2 rounded-md text-white'>View Properties</button>
                 </div>
             </div>
 
@@ -42,6 +55,26 @@ const Home = () => {
                   number={number}
                 />
               ))}
+            </div>
+
+            <Dialog open={isOpen} onOpenChange={setIsOpen}>
+              {/* <DialogTrigger>Open</DialogTrigger> */}
+              <DialogContent>
+                <DialogHeader>
+                  <DialogTitle>Are you absolutely sure?</DialogTitle>
+                  <DialogDescription>
+                    This action cannot be undone. This will permanently delete your account
+                    and remove your data from our servers.
+
+                    {/* <Button onClick={() => setIsOpen(false)}>Close</Button> */}
+                  </DialogDescription>
+                </DialogHeader>
+              </DialogContent>
+            </Dialog>
+
+            {/* last button */}
+            <div className='w-full flex justify-end'>
+              <button className='bg-green-400 px-12 py-3 rounded-md text-white'>Chat with AI</button>
             </div>
         </div>
     </div>
